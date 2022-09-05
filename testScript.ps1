472cb63240fa9e34514937357fbf9f85fa433796
AdnanSharique <48666706+AdnanSharique@users.noreply.github.com>
2022-09-05 17:22:46 +0530

    Update testScript.ps1

--- testScript.ps1
+++ testScript.ps1
@@ -1,2 +1,1 @@
-param( [string] $spId, [string] $tenantId)
-az login --service-principal -u $spId -p 'qez8Q~fCioseNX5u_~cekJj8pBODXumO6Pp5oaKS' --tenant $tenantId
+az login --service-principal -u 'ae549fe0-7514-4da2-a413-d186abbc6fab' -p 'qez8Q~fCioseNX5u_~cekJj8pBODXumO6Pp5oaKS' --tenant 'f94768c8-8714-4abe-8e2d-37a64b18216a'
