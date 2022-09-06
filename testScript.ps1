9b84300b6195ac857d869cf0ac305643d51219e3
AdnanSharique <48666706+AdnanSharique@users.noreply.github.com>
2022-09-06 12:33:50 +0530

    Update testScript.ps1

--- testScript.ps1
+++ testScript.ps1
@@ -1,4 +1,4 @@
-az login -sp\
+az login --service-principal\
 -u 'ae549fe0-7514-4da2-a413-d186abbc6fab'\
--p 'qez8Q~fCioseNX5u_~cekJj8pBODXumO6Pp5oaKS'\
+-p ''\
 --tenant 'f94768c8-8714-4abe-8e2d-37a64b18216a'
